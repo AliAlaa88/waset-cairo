d323665f72dd12b7a425235d1d8c226cc0a49222
omar <141494327+MoMoAymn@users.noreply.github.com>
2024-11-30 18:02:32 +0200

    signin-signup first

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,41 +1,43 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import RootLayout from "./pages/RootLayout";
-
 import Home from "./pages/Home";
-
 import TourPacks from "./pages/TourPacks";
 import TourPackDetails from "./pages/TourPackDetails";
 import Events from "./pages/Events";
 import EventDetails from "./pages/EventDetails";
 import Monuments from "./pages/Monuments";
 import MonumentDetails from "./pages/MonumentDetails";
-
 import ErrorPage from "./pages/Error";
+import Login from "./pages/Login";
+import Signup from "./pages/signup";
 
 function Router() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<RootLayout />}>
-					<Route index element={<Home />} />
-					<Route path="events">
-						<Route index element={<Events />} />
-						<Route path=":id" element={<TourPackDetails />} />
-					</Route>
-					<Route path="tour-packs">
-						<Route index element={<TourPacks />} />
-						<Route path=":id" element={<EventDetails />} />
-					</Route>
-					<Route path="monuments">
-						<Route index element={<Monuments />} />
-						<Route path=":id" element={<MonumentDetails />} />
-					</Route>
-				</Route>
-				<Route path="*" element={<ErrorPage />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+		<Route path="/up" element={<Signup />} />
+        <Route path="/begin" element={<RootLayout />}>
+          <Route index element={<Home />} />
+          <Route path="events">
+            <Route index element={<Events />} />
+            <Route path=":id" element={<TourPackDetails />} />
+          </Route>
+          <Route path="tour-packs">
+            <Route index element={<TourPacks />} />
+            <Route path=":id" element={<EventDetails />} />
+          </Route>
+          <Route path="monuments">
+            <Route index element={<Monuments />} />
+            <Route path=":id" element={<MonumentDetails />} />
+          </Route>
+        </Route>
+
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default Router;
