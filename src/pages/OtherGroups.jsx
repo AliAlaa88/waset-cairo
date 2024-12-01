ea5750afc87904e4edcaf29d7849fba9b876f5fa
HossamMo123 <mah24719782471978@gmail.com>
2024-12-01 12:15:47 +0200

    tourst_groups
    12/1/2024

---
+++ src/pages/OtherGroups.jsx
@@ -0,0 +1,24 @@
+
+
+function Groups(){
+    return (
+        <>
+        <h2 className = "OGroup">Other Tourst Groups</h2>
+        <div className="group_to_join">
+            <div className="LGroup">
+                <div className="group-item">
+                    Group1
+                    <button className="joinButton">join</button>
+                </div>
+                <div className="group-item">
+                Group2
+                <button className="joinButton">join</button>
+                </div>
+            </div>
+        </div>
+        </>
+    );
+
+}
+
+export default Groups;
---
+++ src/pages/myTourstGroup.jsx
@@ -0,0 +1,24 @@
+
+
+function MyGroups(){
+    return (
+        <>
+        <h2 className = "OGroup">My Tourst Groups</h2>
+        <div className="group_to_join">
+            <div className="LGroup">
+                <div className="group-item">
+                    Group1
+                    <button className="joinButton">Open</button>
+                </div>
+                <div className="group-item">
+                Group2
+                <button className="joinButton">Open</button>
+                </div>
+            </div>
+        </div>
+        </>
+    );
+
+}
+
+export default MyGroups;
