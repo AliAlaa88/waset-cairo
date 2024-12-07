d56d2596c0f0ab52075ef3b2d48a31669a17c4b4
Saif <saifeldin402@gmail.com>
2024-12-07 16:43:00 +0200

    set up neon db for backend

--- server/server.js
+++ server/server.js
@@ -1,17 +1,47 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import {Client, neonConfig} from '@neondatabase/serverless'
+import ws from 'ws'
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.json({"msg" : "Hello World"});
+dotenv.config();
+
+neonConfig.webSocketConstructor = ws;
+const client = new Client(process.env.DATABASE_URL);
+client.neonConfig.webSocketConstructor = ws;
+
+(async () => {
+    try {
+        await client.connect();
+        console.log('Connected to Neon PostgreSQL database successfully!');
+    } catch (err) {
+        console.error('Error connecting to Neon database:', err);
+    }
+})();
+
+
+app.get('/api/users', async (req, res) => {
+    try {
+        const result = await client.query('SELECT * FROM users');
+        res.json(result.rows);
+    } catch (err) {
+        console.error('Error fetching data:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}!`);
+});
+
+//close db connection on server shutdown
+process.on('SIGINT', async () => {
+    console.log('Closing database connection...');
+    await client.end();
+    process.exit();
 });
