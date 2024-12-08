686b6610afcb910050928aecc7dadff91ae32600
Saif <saifeldin402@gmail.com>
2024-12-08 16:39:58 +0200

    - added logout and change password to tourist profile. - setted up the full db on neon

--- client/src/pages/CreateGroup.jsx
+++ client/src/pages/CreateGroup.jsx
@@ -1,8 +1,9 @@
-import { useState , useNavigate } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function CreateGroup() {
   const [name, setname] = useState('');
-  const [discribion, setdiscribion] = useState('');
+  const [description, setdescription] = useState('');
   const [language, setlanguage] = useState('');
   const navigate = useNavigate();
   const [selectedMonument, setselectedMonument] = useState("");
@@ -14,7 +15,7 @@ function CreateGroup() {
   "Option 4",
   "Option 5",
 ];
-const handelchange1= (event)=>{
+const handleChange1= (event)=>{
 
   setselectedMonument(event.target.value);
 };
@@ -22,15 +23,15 @@ const handelchange1= (event)=>{
   const Creclik = (event) => {
     event.preventDefault();
 
-    if (!name || !discribion) {
-      alert('Please fill in both group name and discribion');
+    if (!name || !description) {
+      alert('Please fill in both group name and description');
     } else {
       navigate('/home');
     }
   };
 
 
-function Cancle(){
+function Cancel(){
     console.log("return to home page");
     navigate('/home');
 }
@@ -40,7 +41,7 @@ function Cancle(){
     <body className= "cerG-body">
       <div className="create-container">
         <div className="create-box">
-          <h1 className="create-title">Create tourst group</h1>
+          <h1 className="create-title">Create tourist group</h1>
           <form onSubmit={Creclik}>
             <div className="inputs">
               <div className="input">
@@ -65,7 +66,7 @@ function Cancle(){
                 <select className="creG-input" 
                   
                   value={selectedMonument}
-                  onChange={handelchange1}
+                  onChange={handleChange1}
                   >
                       <option value="" disabled>
                           select option
@@ -83,14 +84,14 @@ function Cancle(){
                   type="text"
                   cols="18" rows="5"
                   required 
-                  placeholder="discribion" 
-                  value={discribion} 
-                  onChange={(e) => setdiscribion(e.target.value)} 
+                  placeholder="description" 
+                  value={description} 
+                  onChange={(e) => setdescription(e.target.value)} 
                 />
               </div>
             </div>
             <div className="cbuttons">
-              <button className="cancleBtn" type="button" onClick={() => Cancle()}>Cancle</button>
+              <button className="cancelBtn" type="button" onClick={() => Cancel()}>Cancel</button>
               <button className="submitBtn" type="submit">Create</button>
             </div>
           </form>
--- client/src/pages/guide home page components/Settings.jsx
+++ client/src/pages/guide home page components/Settings.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import { Edit } from 'lucide-react';
 
 const SettingsContent = () => {
     return (
@@ -10,9 +12,11 @@ const SettingsContent = () => {
                     <p className="text-sm text-gray-600">Name: Michael Townley</p>
                     <p className="text-sm text-gray-600">Email: michael@email.com</p>
                     <br/>
-                    <button className="bg-amber-700 text-white px-4 py-2 rounded-lg text-sm hover:bg-amber-800 transition">
-                        Edit Profile
-                    </button>
+                    <Link>
+						<button className="bg-amber-700 text-white px-4 py-2 rounded-full flex items-center mr-4 hover:bg-amber-800">
+							<Edit className="mr-2" size={20} /> Edit Profile
+						</button>
+					</Link>
                 </div>
                 <div>
                     <h3 className="font-semibold text-amber-800 mb-2">Account</h3>
