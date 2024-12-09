522ca378e5502e2753fd2cae1bd1d18fcbbb9452
Ali Alaa <alieldinalaa04@gmail.com>
2024-12-09 23:00:46 +0200

    auth: signup, login, logout, generate token, middleware to protect routes

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -3,8 +3,9 @@ import authController from '../controllers/auth.controller.js'
 import authMiddleware from '../middlewares/auth.middleware.js'
 const authRouter = Router()
 
-authRouter.post('/signUp', authController.signup)
-authRouter.post('/logIn', authController.login)
+authRouter.post('/signup', authController.signup)
+authRouter.post('/login', authController.login)
+// add resetPassword not protected route
 authRouter.post('/newPassword', authMiddleware, authController.updatePassword)
 authRouter.post('/logOut', authMiddleware, authController.logout)
 
