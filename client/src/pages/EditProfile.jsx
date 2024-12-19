4bd3b6e566e488924b557b7dc185c1d3be43cf4a
Ali Alaa <alieldinalaa04@gmail.com>
2024-12-19 12:26:04 +0200

    Edit profile, Change password

--- client/src/pages/EditProfile.jsx
+++ client/src/pages/EditProfile.jsx
@@ -1,104 +1,153 @@
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
-import { useSelector } from "react-redux";
-
+import { useEffect, useState } from "react";
+import { useGetCurrUserDataQuery } from "../store/userSlice";
+import { useTouristEditProfileMutation } from "../store/registrationSlice";
 function EditProfile() {
-  const profileData = useSelector((state) => state.ui.profileData);
+	const { data: profileData, isFetching } = useGetCurrUserDataQuery();
+	const [touristEditProfile] = useTouristEditProfileMutation();
 	const navigate = useNavigate();
+	
+	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
+	const [username, setUsername] = useState("");
+	const [email, setEmail] = useState("");
+	const [birthdate, setBirthdate] = useState("");
+	const [gender, setGender] = useState("");
+	const [bio, setBio] = useState("");
+	const [phoneNo, setPhoneNo] = useState(0);
+	
+	useEffect(() => {
+		if (profileData) {
+			setFirstName(profileData.fname);
+			setLastName(profileData.lname);
+			setUsername(profileData.username);
+			setEmail(profileData.email);
+			const date = new Date(profileData.birthdate).toISOString().split("T")[0];
+			setBirthdate(date);
+			setGender(profileData.gender);
+			setBio(profileData.bio);
+			setPhoneNo(profileData.phonenumber);
+		}
+	}, [profileData]);
 
-	const [firstName, setFirstName] = useState(profileData.firstName);
-	const [lastName, setLastName] = useState(profileData.lastName);
-	const [username, setUsername] = useState(profileData.username);
-	const [email, setEmail] = useState(profileData.email);
-	const [birthdate, setBirthdate] = useState(profileData.birthdate);
-	const [gender, setGender] = useState(profileData.gender);
-
-	function signin(event) {
-    event.preventDefault();
-    // update profile data dispatch here
-		navigate("../");
+	async function signin(event) {
+		event.preventDefault();
+		try {
+			const res = await touristEditProfile({
+				firstName,
+				lastName,
+				username,
+				email,
+				gender,
+				birthdate,
+				bio,
+				phoneNo,
+			}).unwrap();
+			window.location.reload();
+		} catch (err) {
+			console.log(err?.data?.message || err.error);
+		}
 	}
 
 	return (
 		<div className="signup-container">
-			<form className="signup-form" onSubmit={signin}>
-				<h1 className="signup-title">Edit Profile</h1>
-				<div className="signup-input-group">
-					<input
-						className="signup-input"
-						type="text"
-						placeholder="First Name"
-						required
-						value={firstName}
-						onChange={(e) => setFirstName(e.target.value)}
-					/>
-					<input
-						className="signup-input"
-						type="text"
-						placeholder="Last Name"
-						required
-						value={lastName}
-						onChange={(e) => setLastName(e.target.value)}
-					/>
-					<input
-						className="signup-input"
-						type="text"
-						placeholder="Username"
-						required
-						value={username}
-						onChange={(e) => setUsername(e.target.value)}
-					/>
-					<input
-						className="signup-input"
-						type="email"
-						placeholder="E-mail"
-						required
-						value={email}
-						onChange={(e) => setEmail(e.target.value)}
-					/>
-					<label className="signup-label signup-birth-date">
-						<span>Birth Date:</span>
+			{isFetching ? (
+				<p>Loading...</p>
+			) : (
+				<form className="signup-form" onSubmit={signin}>
+					<h1 className="signup-title">Edit Profile</h1>
+					<div className="signup-input-group">
 						<input
 							className="signup-input"
-							type="date"
-							name="birthdate"
+							type="text"
+							placeholder="First Name"
 							required
-							value={birthdate}
-							onChange={(e) => setBirthdate(e.target.value)}
+							value={firstName}
+							onChange={(e) => setFirstName(e.target.value)}
 						/>
-					</label>
-				</div>
-
-				<div className="signup-radio-group">
-					<span>Gender</span>
-					<br />
-					<label>
 						<input
-							type="radio"
-							name="gender"
-							value="male"
+							className="signup-input"
+							type="text"
+							placeholder="Last Name"
 							required
-							checked={gender === "male"}
-							onChange={(e) => setGender(e.target.value)}
+							value={lastName}
+							onChange={(e) => setLastName(e.target.value)}
 						/>
-						Male
-					</label>
-					<label>
 						<input
-							type="radio"
-							name="gender"
-							value="female"
-							checked={gender === "female"}
-							onChange={(e) => setGender(e.target.value)}
+							className="signup-input"
+							type="text"
+							placeholder="Username"
+							required
+							value={username}
+							onChange={(e) => setUsername(e.target.value)}
 						/>
-						Female
-					</label>
-				</div>
+						<input
+							className="signup-input"
+							type="email"
+							placeholder="E-mail"
+							required
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+						<textarea
+							className="signup-input"
+							placeholder="Bio"
+							required
+							value={bio}
+							onChange={(e) => setBio(e.target.value)}
+						/>
+						<input
+							className="signup-input"
+							type="number"
+							placeholder="phonenumber"
+							required
+							value={phoneNo}
+							onChange={(e) => setPhoneNo(e.target.value)}
+						/>
+						<label className="signup-label signup-birth-date">
+							<span>Birth Date:</span>
+							<input
+								className="signup-input"
+								type="date"
+								name="birthdate"
+								required
+								value={birthdate}
+								onChange={(e) => setBirthdate(e.target.value)}
+							/>
+						</label>
+					</div>
 
-				<button type="submit" className="signup-button">
-					Save
-				</button>
-			</form>
+					<div className="signup-radio-group">
+						<span>Gender</span>
+						<br />
+						<label>
+							<input
+								type="radio"
+								name="gender"
+								value="M"
+								required
+								checked={gender === "M"}
+								onChange={(e) => setGender(e.target.value)}
+							/>
+							Male
+						</label>
+						<label>
+							<input
+								type="radio"
+								name="gender"
+								value="F"
+								checked={gender === "F"}
+								onChange={(e) => setGender(e.target.value)}
+							/>
+							Female
+						</label>
+					</div>
+
+					<button type="submit" className="signup-button">
+						Save
+					</button>
+				</form>
+			)}
 		</div>
 	);
 }
